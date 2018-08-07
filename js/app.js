ba18c9860cd2883e2ea7b3622bfb802aa53e787d
Judith Dea <JudithFoe@gmail.com>
2018-08-07 00:25:48 -0400

    initialized the MOV structure

--- js/app.js
+++ js/app.js
@@ -1,17 +1,36 @@
-let allCats = [
-  { name: 'Mieze',
-    picture: 'img/cat1.jpg'
-  },
-  { name: 'Feathers',
-    picture: 'img/cat2'
-  },
-  { name: 'Penelope',
-  picture: 'img/cat3.jpg'
-  },
-  { name: 'Lala',
-    picture: 'img/cat4.jpg'
-  },
-  { name: 'Hank',
-    picture: 'img/cat5.jpg'
-  };
-];
+// the model is holding all the data
+
+let model = {
+  let allCats = [
+    { name: 'Mieze',
+      picture: 'img/cat1.jpg',
+      clickCount: 0
+    },
+    { name: 'Feathers',
+      picture: 'img/cat2',
+      clickCount: 0
+    },
+    { name: 'Penelope',
+      picture: 'img/cat3.jpg',
+      clickCount: 0
+    },
+    { name: 'Lala',
+      picture: 'img/cat4.jpg',
+      clickCount: 0
+    },
+    { name: 'Hank',
+      picture: 'img/cat5.jpg',
+      clickCount: 0
+    }
+  ]
+};
+
+// the octopus is separating and communicating between model and view.
+// It also includes the init function!
+
+let octopus = {
+
+};
+
+
+// View -- actually 2 separate views. One shows the cat list, one shows the cat card
