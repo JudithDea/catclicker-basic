62f0023e28620d96e851d31c732e683df3180df5
Judith Dea <JudithFoe@gmail.com>
2018-08-07 00:19:50 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+let allCats = [
+  { name: 'Mieze',
+    picture: 'img/cat1.jpg'
+  },
+  { name: 'Feathers',
+    picture: 'img/cat2'
+  },
+  { name: 'Penelope',
+  picture: 'img/cat3.jpg'
+  },
+  { name: 'Lala',
+    picture: 'img/cat4.jpg'
+  },
+  { name: 'Hank',
+    picture: 'img/cat5.jpg'
+  };
+];
